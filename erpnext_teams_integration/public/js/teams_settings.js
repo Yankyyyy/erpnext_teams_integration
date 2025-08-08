4eabe4c96fa845db2b2e6cf7d577314bf10f9acd
Yankyyyy <tamocha44@gmail.com>
2025-08-08 11:03:02 +0000

    bug: docname missing for teams settings js script

--- erpnext_teams_integration/public/js/teams_settings.js
+++ erpnext_teams_integration/public/js/teams_settings.js
@@ -6,6 +6,7 @@ frappe.ui.form.on('Teams Settings', {
             frm.add_custom_button(__('Authenticate with Teams'), function() {
                 frappe.call({
                     method: "erpnext_teams_integration.api.helpers.get_login_url",
+                     args: { docname: frm.doc.name },
                     callback: function(r) {
                         if (r.message) {
                             window.location.href = r.message; // redirect to MS login
