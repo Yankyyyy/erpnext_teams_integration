9409214f35ad87ce4a89165f3eda645f3111797a
Yankyyyy <tamocha44@gmail.com>
2025-08-08 10:11:57 +0000

    bug:doctypes missing error

---
+++ erpnext_teams_integration/erpnext_teams_integration/doctype/teams_conversation/teams_conversation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Yanky and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Teams Conversation", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ erpnext_teams_integration/erpnext_teams_integration/doctype/teams_settings/test_teams_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Yanky and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTeamsSettings(FrappeTestCase):
+	pass
